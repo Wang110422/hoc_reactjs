d407ff9fee2746a133ed336bfaddfe0bb7f1bada
Wang110422 <quangoke94@gmail.com>
2025-11-07 21:49:29 +0700

    B2

---
+++ src/router.js
@@ -0,0 +1,37 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/users/homePage";
+import Profile from "./pages/users/profile";
+import { ROUTERS } from "./utils/router";
+import MasterLayout from "./pages/users/theme/masterLayout";
+
+
+const renderUserRouter = () => {
+    const userRouters = [{
+        path: ROUTERS.USER.HOME,
+        component: <HomePage />
+    },
+    {
+        path: ROUTERS.USER.PROFILE,
+        component: <Profile />
+    }
+    ]
+    return (
+        <MasterLayout>
+            <Routes>
+                {
+                    userRouters.map(
+                        (item, index) => {
+                            return <Route key={index} path={item.path} element={item.component} />
+                        }
+                    )
+                }
+            </Routes>
+        </MasterLayout>
+
+    )
+}
+const RouterCustom = () => {
+    return renderUserRouter();
+};
+
+export default RouterCustom;
